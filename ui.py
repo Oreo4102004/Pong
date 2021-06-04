bf420e6c4ecbe27965a83e7aa57507f4ed49cec5
gautam <gautamthegreat21@gmail.com>
2021-06-03 17:23:06 -0700

    added multiplayer

--- ui.py
+++ ui.py
@@ -1,5 +1,4 @@
 import tkinter
-from tkinter.constants import W
 
 win = tkinter.Tk()
 win.title("Pong")
@@ -11,8 +10,18 @@ def single():
     if __name__ == "__main__":
         singleplayer
 def multi():
-    
-multiplayer = tkinter.Button(text="Multiplayer", width=42,bg="gray",fg="pink")
+    win.destroy()
+    multi_win = tkinter.Tk()
+    host=tkinter.Label(multi_win,text="IPaddress:").grid(row=0)
+    port=tkinter.Label(multi_win, text='Port:').grid(row=1)
+    e1 = tkinter.Entry(multi_win)
+    e2 = tkinter.Entry(multi_win)
+    e1.grid(row=0, column=1)
+    e2.grid(row=1, column=1)
+    multi_win.mainloop()
+
+
+multiplayer = tkinter.Button(text="Multiplayer", width=42,bg="gray",fg="pink",command=multi)
 single = tkinter.Button(text="Singleplayer", width=42,command=single,bg="gray",fg="pink")
 multiplayer.grid(row=0, column=0)
 single.grid(row=1,column=0)
